945cafe67a25b0697b5a645d09f7ca0144b80d12
Yuta Gunji <yuta.g05@gmail.com>
2020-01-20 14:36:20 +0900

    Setup for factory-bot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,4 +60,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # factory_bot
+  config.include FactoryBot::Syntax::Methods
+
+  config.before(:all) do
+    FactoryBot.reload
+  end
 end
