0707253e2cc8b06044b8078e3b79bbfc6d06cc51
Yuta Gunji <yuta.g05@gmail.com>
2020-01-23 18:04:10 +0900

    Add validations of user with callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  validates :name, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true,
+                       length: { minimum: 6 }
+
+  before_save :downcase_email
+
+  has_secure_password
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
 end
