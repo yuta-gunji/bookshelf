4328c7290d0c15c6f1a4bdeb9bae586c8fdce639
Yuta Gunji <yuta.g05@gmail.com>
2020-01-26 00:36:52 +0900

    Fix helper method in for system spec

--- spec/support/helpers/system_spec_helper.rb
+++ spec/support/helpers/system_spec_helper.rb
@@ -11,10 +11,7 @@ module SystemSpecHelper
 
   def login!
     user = create(:user)
-    visit login_path
-    fill_in User.human_attribute_name(:email), with: user.email
-    fill_in User.human_attribute_name(:password), with: 'password'
-    click_button I18n.t(:login)
+    login_as(user)
   end
 
   def logout
