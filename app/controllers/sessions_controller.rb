c2c0be025844ffa6e90eb5ab2c4ad7c67d6b2499
Yuta Gunji <yuta.g05@gmail.com>
2020-01-25 17:30:40 +0900

    Add feature of login and logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: session_params[:email])
+    if user&.authenticate(session_params[:password])
+      login(user)
+      flash[:success] = I18n.t(:login_succeeded)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    reset_session
+    flash[:success] = I18n.t(:logout_succeeded)
+    redirect_to root_path
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(
+      :email,
+      :password,
+    )
+  end
+
+  def login(user)
+    session[:user_id] = user.id
+  end
+end
