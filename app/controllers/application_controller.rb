a0134f93b67c4ece894e8158656714f456a0f712
Yuta Gunji <yuta.g05@gmail.com>
2020-01-25 17:53:03 +0900

    Add helper methods to use in views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in_user?
+
+  private
+
+  def current_user
+    return nil unless session[:user_id]
+
+    @_current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in_user?
+    !current_user.nil?
+  end
 end
