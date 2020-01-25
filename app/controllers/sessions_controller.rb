52f18ee18057ad870ff9c439ddb6a8be0cc47103
Yuta Gunji <yuta.g05@gmail.com>
2020-01-25 18:23:23 +0900

    Add guard logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,8 @@
 
 class SessionsController < ApplicationController
   def new
+    flash[:info] = I18n.t(:already_logged_in)
+    return redirect_to root_path if logged_in_user?
   end
 
   def create
