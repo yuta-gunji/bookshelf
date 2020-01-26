31b00f85964ec33481b431f7a607738e3f27bc1e
Yuta Gunji <yuta.g05@gmail.com>
2020-01-26 15:13:32 +0900

    Add remember and forget feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,15 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    return nil unless session[:user_id]
-
-    @_current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @_current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user&.authenticated?(cookies[:remember_token])
+        login(user)
+        @_current_user = user
+      end
+    end
   end
 
   def logged_in_user?
