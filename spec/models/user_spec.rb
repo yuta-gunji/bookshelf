0617b86cbeee1ace1de6f7170f61605aeb9319ab
Yuta Gunji <yuta.g05@gmail.com>
2020-01-27 16:46:18 +0900

    Update rspec test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -97,4 +97,37 @@ RSpec.describe User, type: :model do
     encrypted_password = User.digest('password')
     it { expect(encrypted_password).to be_an_instance_of(BCrypt::Password) }
   end
+
+  describe '.new_token' do
+    it { expect(User.new_token).to be_an_instance_of(String) }
+  end
+
+  describe '#remember' do
+    before do
+      user.remember
+    end
+    it { expect(user.remember_digest).to be_truthy }
+  end
+
+  describe '#authenticated?' do
+    context 'when remember_digest exists' do
+      let(:remember_token) { new_token }
+      let(:user_with_digest) { create(:user, remember_digest: digest(remember_token)) }
+      it { expect(user_with_digest.authenticated?(remember_token)).to be_truthy }
+    end
+
+    context 'when remember_digest is empty' do
+      let(:remember_token) { new_token }
+      let(:user_without_digest) { create(:user, remember_digest: nil) }
+      it { expect(user.authenticated?(remember_token)).to be_falsey }
+    end
+  end
+
+  describe '#forget' do
+    let(:user_with_digest) { create(:user, remember_digest: digest(new_token)) }
+    before do
+      user.forget
+    end
+    it { expect(user.remember_digest).to be_falsey }
+  end
 end
--- spec/system/login_spec.rb
+++ spec/system/login_spec.rb
@@ -15,4 +15,22 @@ RSpec.feature 'Login', type: :system do
     expect(page).to have_link I18n.t(:logout), href: logout_path
     expect(page).not_to have_link I18n.t(:login), href: login_path
   end
+
+  scenario 'login with valid information followed by multi logout process' do
+    login!
+    click_link I18n.t(:logout), href: logout_path
+    expect(page).to have_link I18n.t(:login), href: login_path
+    page.driver.submit :delete, '/logout', {} # It is assumed logout process from another tab in the same browser.
+    expect(page).to have_link I18n.t(:login), href: login_path
+  end
+
+  scenario 'login with remembering' do
+    login_as(user, remember: '1')
+    expect(page.driver.browser.rack_mock_session.cookie_jar[:remember_token]).to be_truthy
+  end
+
+  scenario 'login without remembering' do
+    login_as(user, remember: '0')
+    expect(page.driver.browser.rack_mock_session.cookie_jar[:remember_token]).to be_falsey
+  end
 end
