240418033fc2b888dd83dcfdd39529dbc84da5dc
Yuta Gunji <yuta.g05@gmail.com>
2020-01-28 16:32:08 +0900

    Fix system test for readability

--- spec/system/signup_spec.rb
+++ spec/system/signup_spec.rb
@@ -3,14 +3,19 @@
 require 'rails_helper'
 
 RSpec.feature 'Signup', type: :system do
-  scenario 'is processed correctly' do
+  let(:valid_email) { 'example@example.com' }
+  let(:invalid_email) { 'invailid_email@' }
+  let(:password) { 'password' }
+  let(:another_password) { 'another_password' }
+
+  scenario 'signup with valid information' do
     visit signup_path
     expect(page).to have_link 'こちら', href: login_path
 
     fill_in User.human_attribute_name(:name), with: 'yuta'
-    fill_in User.human_attribute_name(:email), with: 'example@example.com'
-    fill_in User.human_attribute_name(:password), with: 'password'
-    fill_in User.human_attribute_name(:password_confirmation), with: 'password'
+    fill_in User.human_attribute_name(:email), with: valid_email
+    fill_in User.human_attribute_name(:password), with: password
+    fill_in User.human_attribute_name(:password_confirmation), with: password
     expect { click_button I18n.t(:register) }.to change { User.count }.by(1)
 
     expect(ActionMailer::Base.deliveries.size).to eq 1
@@ -19,12 +24,12 @@ RSpec.feature 'Signup', type: :system do
     expect(page).not_to have_link I18n.t(:logout), href: logout_path
   end
 
-  scenario 'has some errors' do
+  scenario 'signup with invalid information' do
     visit signup_path
     fill_in User.human_attribute_name(:name), with: 'yuta'
-    fill_in User.human_attribute_name(:email), with: 'invailid_email@'
-    fill_in User.human_attribute_name(:password), with: 'password'
-    fill_in User.human_attribute_name(:password_confirmation), with: 'another_password'
+    fill_in User.human_attribute_name(:email), with: invalid_email
+    fill_in User.human_attribute_name(:password), with: password
+    fill_in User.human_attribute_name(:password_confirmation), with: another_password
     expect { click_button I18n.t(:register) }.not_to change(User, :count)
 
     expect(current_path).to eq signup_path
