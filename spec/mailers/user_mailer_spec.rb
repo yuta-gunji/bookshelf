4109ad376213c6a6070463dbe6e0e6562077dc63
Yuta Gunji <yuta.g05@gmail.com>
2020-01-30 00:01:01 +0900

    Add UserMailer#password_reset with views

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -13,4 +13,27 @@ RSpec.describe UserMailer, type: :mailer do
       expect(mail.from).to eq(['noreply@example.com'])
     end
   end
+
+  describe 'password_reset' do
+    let(:user) { create(:user) }
+    let(:mail) { UserMailer.password_reset(user) }
+
+    before do
+      user.reset_token = User.new_token
+    end
+
+    it 'renders headers' do
+      expect(mail.subject).to eq(I18n.t(:password_reset))
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(['noreply@example.com'])
+    end
+
+    it 'renders body' do
+      expect(mail.body.parts.collect(&:content_type)).to include 'text/html; charset=UTF-8', 'text/plain; charset=UTF-8'
+      expect(mail.body.parts.find { |p| p.content_type.match(/html/) }.body.raw_source).to match(user.reset_token)
+      expect(mail.body.parts.find { |p| p.content_type.match(/html/) }.body.raw_source).to match(CGI.escape(user.email))
+      expect(mail.body.parts.find { |p| p.content_type.match(/plain/) }.body.raw_source).to match(user.reset_token)
+      expect(mail.body.parts.find { |p| p.content_type.match(/plain/) }.body.raw_source).to match(CGI.escape(user.email))
+    end
+  end
 end
