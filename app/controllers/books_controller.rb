3dfea0a9e5528ca9c46e00a2093aa33e296bf57f
Yuta Gunji <yuta.g05@gmail.com>
2020-02-05 22:23:06 +0900

    Add books#search

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,5 +2,12 @@
 
 class BooksController < ApplicationController
   def search
+    if (@keyword = params[:keyword]).present?
+      request = GoogleBooks::Request.new(params[:keyword])
+      response = request.submit
+      @books = response.found_books
+    else
+      @books = []
+    end
   end
 end
