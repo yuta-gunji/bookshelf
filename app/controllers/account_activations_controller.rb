1d26400d692756379a58cfe019b50ba5cd4a7e73
Yuta Gunji <yuta.g05@gmail.com>
2020-02-06 16:08:49 +0900

    Create user's bookshelf when account acrtivation

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -5,11 +5,18 @@ class AccountActivationsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
+      create_bookshelf(user)
       login(user)
       flash[:success] = I18n.t(:successfully_activated)
     else
       flash[:danger] = I18n.t(:invalid_activation_link)
     end
     redirect_to root_path
+  end
+
+  private
+
+  def create_bookshelf(user)
+    user.create_bookshelf(name: "#{user.name}さんの本棚")
   end
 end
