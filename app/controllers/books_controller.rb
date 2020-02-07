66ca73bcd3ce72b29426bb67909cb921de975ecd
Yuta Gunji <yuta.g05@gmail.com>
2020-02-07 21:48:34 +0900

    Reject already added books from results of search

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,8 @@ class BooksController < ApplicationController
     if (@keyword = params[:keyword]).present?
       request = GoogleBooks::Request.new(params[:keyword])
       response = request.submit
-      @books = response.found_books
+      added_google_books_ids = current_user.bookshelf.books.pluck(:google_books_id)
+      @books = response.found_books.reject { |book| added_google_books_ids.include?(book.book_id) }
     else
       @books = []
     end
