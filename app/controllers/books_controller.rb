d23b1f97bd46fbeecbecb75efa9d72928147d043
Yuta Gunji <yuta.g05@gmail.com>
2020-02-09 15:01:56 +0900

    Add feature of books#index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
 class BooksController < ApplicationController
+  include ActiveRecord::Sanitization::ClassMethods
+
+  def index
+    @books =
+      if (@keyword = params[:keyword].downcase)
+        Book.where('lower(title) LIKE ?', "%#{sanitize_sql_like(@keyword)}%").order(created_at: :desc)
+      else
+        Book.order(created_at: :desc)
+      end
+  end
+
   def search
     if (@keyword = params[:keyword]).present?
       request = GoogleBooks::Request.new(params[:keyword])
