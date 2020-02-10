77439f006c6f43a7c846803609ed204f5bcc75d1
Yuta Gunji <yuta.g05@gmail.com>
2020-02-10 10:52:54 +0900

    Fix request test of books#index

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -4,12 +4,22 @@ require 'rails_helper'
 
 RSpec.describe 'Books', type: :request do
   describe 'GET #index' do
-    let(:title) { 'awesome title' }
-    let(:book) { create(:book, title: title) }
+    let(:title_1) { 'awesome title' }
+    let(:title_2) { 'another awesome title' }
+    before do
+      create(:book, title: title_1)
+      create(:book, title: title_2)
+    end
+
     it 'succeeds' do
       get books_path
       expect(response.status).to eq 200
-      expect(response.body).to include title
+    end
+
+    it 'displays titles of books' do
+      get books_path
+      expect(response.body).to include title_1
+      expect(response.body).to include title_2
     end
   end
 end
