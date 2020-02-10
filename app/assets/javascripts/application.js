ecb4e57db80381db5a16afa333e501ef31bb4ccb
Yuta Gunji <yuta.g05@gmail.com>
2020-02-10 20:53:54 +0900

    Import custom js for material-design-for-bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,4 +2,5 @@
 //= require popper.js/dist/umd/popper
 //= require popper.js/dist/umd/popper-utils
 //= require bootstrap-material-design/dist/js/bootstrap-material-design
+//= require bootstrap_material_design
 //= require @fortawesome/fontawesome-free/js/all
