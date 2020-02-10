e3f2dbfa541ae8f5618128dcb9081a17831df2f7
Yuta Gunji <yuta.g05@gmail.com>
2020-02-10 11:23:59 +0900

    Add request test of books#show

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -22,4 +22,25 @@ RSpec.describe 'Books', type: :request do
       expect(response.body).to include title_2
     end
   end
+
+  describe 'GET #show' do
+    let(:book) { create(:book) }
+
+    context 'when book exists' do
+      it 'succeeds' do
+        get book_path(book.id)
+        expect(response.status).to eq 200
+      end
+
+      it 'displays title of target book' do
+        get book_path(book.id)
+        expect(response.body).to include book.title
+      end
+    end
+
+    context 'when book does not exist' do
+      subject { -> { get book_path(1) } }
+      it { is_expected.to raise_error ActiveRecord::RecordNotFound }
+    end
+  end
 end
