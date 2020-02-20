2e252446c9dbce018f5509dc03e029da458e8a7a
Yuta Gunji <yuta.g05@gmail.com>
2020-02-20 16:16:16 +0900

    No use of sprockets

--- Gemfile
+++ Gemfile
@@ -15,7 +15,6 @@ gem 'jbuilder', '~> 2.7'
 gem 'pg', '>= 0.18', '< 2.0'
 gem 'puma', '~> 4.1'
 gem 'rails-i18n'
-gem 'sassc-rails'
 gem 'slim-rails'
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 gem 'webpacker', '~> 4.0'
--- app/assets/javascripts/application.js
+++
@@ -1,6 +0,0 @@
-//= require jquery/dist/jquery
-//= require popper.js/dist/umd/popper
-//= require popper.js/dist/umd/popper-utils
-//= require bootstrap-material-design/dist/js/bootstrap-material-design
-//= require bootstrap_material_design
-//= require @fortawesome/fontawesome-free/js/all
