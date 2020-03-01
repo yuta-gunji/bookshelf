fde6cb300e1481f99bee32a286cb70940e112ee3
Yuta Gunji <yuta.g05@gmail.com>
2020-03-01 23:49:32 +0900

    Setup for raty-js

--- app/frontend/javascripts/application.js
+++ app/frontend/javascripts/application.js
@@ -1,6 +1,19 @@
 import 'bootstrap-material-design';
 import '@fortawesome/fontawesome-free/js/all';
+import 'raty-js';
+import starOn from "../images/star-on.png";
+import starOff from "../images/star-off.png";
 
 $(document).ready(function () {
   $('body').bootstrapMaterialDesign();
+
+  // For raty-js
+  $('.new_rating').raty(
+    {
+      starOn: starOn,
+      starOff: starOff,
+      score: 3,
+      scoreName: 'review[rate]'
+    }
+  )
 })
