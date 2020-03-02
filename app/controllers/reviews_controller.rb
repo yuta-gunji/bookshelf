db652011297c69bb871b337ec3cab9b6da4fa948
Yuta Gunji <yuta.g05@gmail.com>
2020-03-02 17:45:04 +0900

    Add feature of reviews#edit, #update

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 class ReviewsController < ApplicationController
-  before_action :authenticate_user!, only: %i[create]
+  before_action :authenticate_user!, only: %i[create edit update]
 
   def create
     set_book
-
+    add_to_bookshelf(@book)
     @review = current_user.reviews.new(review_params)
+
     if @review.save
       flash[:success] = I18n.t(:successfully_created)
       redirect_to book_path(@book)
@@ -14,6 +15,23 @@ class ReviewsController < ApplicationController
       set_reviews_count
       set_adding_status
       render 'books/show'
+    end
+  end
+
+  def edit
+    @review = Review.includes(:book, :user).find(params[:id])
+    redirect_to root_path unless current_user.id == @review.user_id
+  end
+
+  def update
+    @review = Review.includes(:book, :user).find(params[:id])
+    redirect_to root_path unless current_user.id == @review.user_id
+
+    if @review.update(review_params)
+      flash[:success] = I18n.t(:successfully_updated)
+      redirect_to book_path(@review.book)
+    else
+      render :edit
     end
   end
 
@@ -26,6 +44,10 @@ class ReviewsController < ApplicationController
       :rate,
       :book_id
     ).merge(reviewed_at: Time.current)
+  end
+
+  def add_to_bookshelf(book)
+    current_user.bookshelf.add!(book)
   end
 
   def set_book
