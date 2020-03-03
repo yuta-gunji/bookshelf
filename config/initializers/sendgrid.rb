acd1b5d56f428676576249b173fe914594e5bc06
Yuta Gunji <yuta.g05@gmail.com>
2020-03-03 16:45:17 +0900

    Setup for sendgrid

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -1,11 +1,3 @@
 # frozen_string_literal: true
 
-ActionMailer::Base.smtp_settings = {
-  user_name: ENV['SENDGRID_USER_NAME'],
-  password: ENV['SENDGRID_PASSWORD'],
-  domain: ENV['DOMAIN_NAME'],
-  address: 'smtp.sendgrid.net',
-  port: 587,
-  authentication: :plain,
-  enable_starttls_auto: true
-}
+ActionMailer::Base.add_delivery_method :sendgrid, Mail::SendGrid, api_key: ENV.fetch('SENDGRID_API_KEY')
