e03ba0b181c798e348f7f3ceff1cb767d7a4d82e
Yuta Gunji <yuta.g05@gmail.com>
2020-03-07 15:55:10 +0900

    Move method for setting name of bookshelf to model

--- app/models/bookshelf.rb
+++ app/models/bookshelf.rb
@@ -5,8 +5,16 @@ class Bookshelf < ApplicationRecord
   has_many :books, through: :bookshelf_books
   belongs_to :user
 
+  before_create :set_name
+
   def add!(book)
     record = books.find_by(id: book.id)
     books << book unless record
+  end
+
+  private
+
+  def set_name
+    self.name = user.name + 'さんの本棚'
   end
 end
