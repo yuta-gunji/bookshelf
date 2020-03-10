192bf79a7a5409cccedb1c23beaf71f3134cfa34
Yuta Gunji <yuta.g05@gmail.com>
2020-03-10 15:50:30 +0900

    Add flash message when found books is empty

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -31,6 +31,7 @@ class BooksController < ApplicationController
       if logged_in_user?
         added_google_books_ids = current_user.bookshelf.books.pluck(:google_books_id)
         @books = @books.reject { |book| added_google_books_ids.include?(book.google_books_id) }
+        flash[:info] = I18n.t(:result_is_empty) unless @books.present?
       end
     else
       @books = []
