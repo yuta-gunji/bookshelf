47be8245b523a6843cce968e5237839cda612b65
Yuta Gunji <yuta.g05@gmail.com>
2020-03-14 21:13:28 +0900

    Delete sample page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get '/sample', to: 'sample#index'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
