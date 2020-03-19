bc7cbe04cd1a48414a807dfcea4fe32be4eba2e3
Yuta Gunji <yuta.g05@gmail.com>
2020-03-19 17:13:25 +0900

    Add port for puma

--- config/puma.rb
+++ config/puma.rb
@@ -14,6 +14,7 @@ threads min_threads_count, max_threads_count
 #
 environment ENV.fetch('RAILS_ENV') { 'development' }
 
+port ENV.fetch('PORT') { 3000 }
 app_root = File.expand_path('..', __dir__)
 bind "unix://#{app_root}/tmp/sockets/puma.sock"
 
