30834cec442c64592276b16eb0a1ac02c6b574c7
Yuta Gunji <yuta.g05@gmail.com>
2020-03-21 14:13:20 +0900

    Update config of prodcution to specify host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "my_bookshelf_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'www.mybkshlf.net' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
